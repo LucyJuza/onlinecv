74cbdc5b0316edfb595a0886ea360f1f3fef53c4
NoeJuza <noe.juzan.pro@gmail.com>
2023-08-23 19:33:10 +0200

    added fiverr

--- front/src/ContactMe.js
+++ front/src/ContactMe.js
@@ -9,6 +9,7 @@ function ContactMe() {
                     <div class="lnkEl"><a target="_blank" rel="noreferrer" href="https://linkedin.com/in/noé-juzan-a3aa88210"><img src={'./svgs/linkedin.svg'} alt="linkedin"/></a></div>
                     <div class="lnkEl"><a target="_blank" rel="noreferrer" href="https://github.com/NoeJuza"><img src={'./svgs/github.svg'} alt="github"/></a></div>
                     <div class="lnkEl"><a href="mailto:noe.juzan.pro@gmail.com"><img class="mail" src={"./svgs/mail.svg"} alt="mail"/></a></div>
+                    <div class="lnkEl"><a target="_blank" rel="noreferrer" href="https://fr.fiverr.com/noejuza"><img src={'./svgs/fiverr.svg'} alt="fiverr"/></a></div>
                 </div>
           </div>
         </div>
