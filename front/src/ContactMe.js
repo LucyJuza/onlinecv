58701e69b8eab5c5e74b3a71340d20453058d408
lucie.juzan <lucie@juzan.dev>
2024-08-19 16:02:44 +0200

    updated gender stuff, name, logo and titles

--- front/src/ContactMe.js
+++ front/src/ContactMe.js
@@ -6,9 +6,9 @@ function ContactMe() {
           <div className="contact">
                 <h1>{string.contactMe[window.userLang]}</h1>
                 <div className="contLink">
-                    <div class="lnkEl"><a target="_blank" rel="noreferrer" href="https://linkedin.com/in/noé-juzan-a3aa88210"><img src={'./svgs/linkedin.svg'} alt="linkedin"/></a></div>
+                    <div class="lnkEl"><a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/lucie-juzan-a3aa88210/"><img src={'./svgs/linkedin.svg'} alt="linkedin"/></a></div>
                     <div class="lnkEl"><a target="_blank" rel="noreferrer" href="https://github.com/LucyJuza"><img src={'./svgs/github.svg'} alt="github"/></a></div>
-                    <div class="lnkEl"><a href="mailto:noe.juzan.pro@gmail.com"><img class="mail" src={"./svgs/mail.svg"} alt="mail"/></a></div>
+                    <div class="lnkEl"><a href="mailto:lucie@juzan.dev"><img class="mail" src={"./svgs/mail.svg"} alt="mail"/></a></div>
                     <div class="lnkEl"><a target="_blank" rel="noreferrer" href="https://fr.fiverr.com/noejuza"><img src={'./svgs/fiverr.svg'} alt="fiverr"/></a></div>
                 </div>
           </div>
--- front/src/LangList.js
+++ front/src/LangList.js
@@ -4,10 +4,10 @@ import Lang from "./Lang";
 const langSet = [
     {"name":"french",
     "fullName":"Français",
-    "text":"Ma langue natale est le français, c'est avec celle-ci que j'ai été elevé et avec laquelle j'ai suivi mon cursus scolaire."},
+    "text":"Ma langue natale est le français, c'est avec celle-ci que j'ai été elevée et avec laquelle j'ai suivi mon cursus scolaire."},
     {"name":"english",
     "fullName":"Anglais",
-    "text":"I am fluent in English as well, allowing me to actively collaborate with native English speakers, as well as the quick reading of English texts and documentations which is a huge help in the development and IT world."},
+    "text":"I am fluent in English as well, allowing me to actively collaborate with native English speakers, as well as the quick reading of English texts and documentations which is a huge help in the programming and IT world."},
     {"name":"german",
     "fullName":"Allemand",
     "text":"Ich habe die Berufsmatura in Deutsch (B2-Äquivalent) absolviert und kann mich mündlich verständigen, auch wenn dies im Falle von Schweizerdeutsch und anderen Dialekten problematisch ist, da es sich nicht mehr um dieselbe Sprache handelt."}]
